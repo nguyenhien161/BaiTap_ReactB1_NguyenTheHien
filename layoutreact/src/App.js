b61b0f667c440bc39db0f97e690880c2c7db4473
nguyenhien161 <lukenguyen161@gmail.com>
2022-08-17 07:54:10 +0700

    upload code

---
+++ layoutreact/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/BTlayout/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
